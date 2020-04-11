79fd5e2a2e34e37cbf81f8827581dc00b62cfc0a
Sergey Alaev <scf370@gmail.com>
2020-04-11 15:04:12 +0300

    disallow multiple instances of layout switcher, they will conflict

--- layout_switcher.sh
+++ layout_switcher.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+ps ax | grep '[l]ayout_switcher.py' > /dev/null 2>&1
+if [ "$?" = "0"  ]; then
+  echo "already running"
+  exit 1
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
